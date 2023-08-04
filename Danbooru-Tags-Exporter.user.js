16b12e6d85f4add820a0987c4c5462119975463e
acceleration3 <acceleration23@gmail.com>
2023-08-04 23:27:52 +0100

    Gelbooru version.

--- Danbooru-Tags-Exporter.user.js
+++ Danbooru-Tags-Exporter.user.js
@@ -1,34 +1,44 @@
 // ==UserScript==
-// @name         Danbooru Tags Select to Export
-// @name:zh-TW   Danbooru 標籤導出器
-// @name:zh-HK   Danbooru 標籤導出器
-// @name:zh-CN   Danbooru 标签导出器
-// @name:ja      Danbooru Tags Select to Export
-// @namespace    https://github.com/FSpark/Danbooru-Tags-Exporter
-// @supportURL   https://github.com/FSpark/Danbooru-Tags-Exporter/issues
-// @homepageURL  https://github.com/FSpark/Danbooru-Tags-Exporter
-// @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @version      0.3.3
+// @name         Gelbooru Tags Select to Export
+// @name:zh-TW   Gelbooru 標籤導出器
+// @name:zh-HK   Gelbooru 標籤導出器
+// @name:zh-CN   Gelbooru 标签导出器
+// @name:ja      Gelbooru Tags Select to Export
+// @namespace    https://github.com/acceleration3/Danbooru-Tags-Exporter
+// @supportURL   https://github.com/acceleration3/Danbooru-Tags-Exporter/issues
+// @homepageURL  https://github.com/acceleration3/Danbooru-Tags-Exporter
+// @version      0.3.2
 // @description  Select specified tags and copy to clipboard, for Stable Diffusion WebUI or NovelAI to use.
 // @description:zh-TW  選擇指定標籤並複製到剪貼板，供Stable Diffusion WebUI或NovelAI等使用
 // @description:zh-HK  選擇指定標籤並複製到剪貼板，供Stable Diffusion WebUI或NovelAI等使用
 // @description:zh-CN  选择指定标签并复制到剪贴板，供Stable Diffusion WebUI或NovelAI等使用
 // @description:ja  指定したタグを選択し、クリップボードにコピーして、Stable Diffusion WebUIやNovelAIなどで使用することができます。
-// @author       FSpark
-// @match        https://danbooru.donmai.us/posts/*
-// @match        https://safebooru.donmai.us/posts/*
-// @match        https://aibooru.online/posts/*
-// @match        https://betabooru.donmai.us/posts/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=donmai.us
-// @grant        GM.setClipboard
-// @grant        GM.notification
-// @grant        GM.addStyle
+// @author       acceleration3
+// @match        https://gelbooru.com/index.php?page=post&s=view&id=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gelbooru.com
+// @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM_addStyle
 // @license      AGPL-3.0
 // ==/UserScript==
 
 (function () {
     'use strict';
-    GM.addStyle(`#tags-exporter-setting button, #tag-list button {padding: 0.25em 0.75em;}
+    if (!GM_addStyle) {
+        var GM_addStyle = function (aCss) {
+            'use strict';
+            let head = document.getElementsByTagName('head')[0];
+            if (head) {
+                let style = document.createElement('style');
+                style.setAttribute('type', 'text/css');
+                style.textContent = aCss;
+                head.appendChild(style);
+                return style;
+            }
+            return null;
+        };
+    }
+    GM_addStyle(`#tags-exporter-setting button, #tag-list button {padding: 0.25em 0.75em;}
                 #tags-exporter-setting label{margin: .25em; line-height: 1.5em;}
                .tag-weight {width: 3em; margin-left: .25em}`);
 
@@ -42,7 +52,7 @@
  <input type="radio" name="use-bracket" id="round-brackets" value="0" checked/><label for="round-brackets">Using ( )</label>
 <input type="radio" name="use-bracket" id="curly-brackets" value="-1"/><label for="curly-brackets">Using { }</label></div>
     `
-	let Container = document.createElement('div');
+    let Container = document.createElement('div');
     Container.id = "tags-exporter-container";
     Container.innerHTML = `<button name="select_all">All</button>
 <button  name="select_none">None</button>
@@ -50,63 +60,68 @@
 <button  name="export">Export</button>
 `
 
-	function insertAfter(newNode, referenceNode) {
+    function insertAfter(newNode, referenceNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
     }
 
-    insertAfter(SettingPanel, document.querySelector("#search-box"))
-    insertAfter(Container, document.querySelector("#tags-exporter-setting > h2"))
-
-    function addBrackets(prompts,isRound,n){
-        let l,r;
-        if(n==0)
-            return prompts
-        else if(n>0){
-            if(isRound){
-                l='('
-                r=')'}
-            else{
-                l='{'
-                r='}'
-            }
-        }else{
-            l='['
-            r=']'
-        }
-        n=Math.abs(n)
-        return l.repeat(n).concat(prompts,r.repeat(n))
+    function insertBefore(newNode, referenceNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode);
     }
-    function exportTags(target){
+
+    insertBefore(SettingPanel, document.querySelector("#tag-list"))
+    insertAfter(Container, document.querySelector("#tags-exporter-setting"))
+
+    function addBrackets(prompts, isRound, n) {
+        let l, r;
+        if (n == 0)
+            return prompts
+        else if (n > 0) {
+            if (isRound) {
+                l = '('
+                r = ')'
+            }
+            else {
+                l = '{'
+                r = '}'
+            }
+        } else {
+            l = '['
+            r = ']'
+        }
+        n = Math.abs(n)
+        return l.repeat(n).concat(prompts, r.repeat(n))
+    }
+    function exportTags(target) {
         let tags = []
         let bracket_escape = document.getElementById("bracket-escape").checked
         let set_weight = document.getElementById("set-weight").checked
         let round_brackets = document.getElementById("round-brackets").checked
         document.querySelectorAll(target).forEach((e) => {
             let prompts = e.value
-            if(bracket_escape)
-                prompts = prompts.replaceAll(`(`,`\\(`).replaceAll(`)`,`\\)`)
-            if(set_weight){
-                prompts = addBrackets(prompts,round_brackets,e.nextSibling.value)
+            if (bracket_escape)
+                prompts = prompts.replaceAll(`(`, `\\(`).replaceAll(`)`, `\\)`)
+            if (set_weight) {
+                prompts = addBrackets(prompts, round_brackets, e.nextSibling.value)
             }
             tags.push(prompts)
         })
         let res = tags.join(", ")
-        GM.setClipboard(res)
-        GM.notification(`${tags.length} tag(s) were copied.`, "Danbooru Tags Exporter")
+        GM_setClipboard(res)
+        GM_notification(`${tags.length} tag(s) were copied.`, "Gelbooru Tags Exporter")
     }
 
-    function insertButtons(target){
-        let head = document.querySelector(`h3.${target}-list`)
-        if(!head) return;
+    function insertButtons(target) {
+        let head = document.querySelector(`li.tag-type-${target}`)
+        if (!head) return;
         let buttonContainer = Container.cloneNode(true)
         buttonContainer.id = `${target}-buttons`
-		insertAfter(buttonContainer, head)
-        document.querySelectorAll(`.${target}-list>li`).forEach((e) => {
+        insertBefore(buttonContainer, head)
+        document.querySelectorAll(`li.tag-type-${target}>a`).forEach((e) => {
             let chk = document.createElement('input');
             chk.type = "checkbox"
             chk.name = `${target}s`
-			chk.value = e.dataset.tagName.replaceAll("_", " ")
-            e.insertBefore(chk, e.firstChild)
+            chk.value = e.innerText
+            insertAfter(chk, e.previousSibling)
 
             let nbr = document.createElement('input');
             nbr.type = "number"
@@ -114,7 +129,7 @@
             nbr.className = "tag-weight"
             nbr.value = 0
             nbr.hidden = true
-            insertAfter(nbr,chk)
+            insertAfter(nbr, chk)
         })
 
         buttonContainer.querySelector("[name='select_all']").onclick = function () {
@@ -143,8 +158,7 @@
         };
     }
 
-    ["artist-tag","character-tag","copyright-tag","general-tag"].forEach((t)=>insertButtons(t))
-
+    ["artist", "metadata", "character", "copyright", "general"].forEach((t) => insertButtons(t))
 
     Container.querySelector("[name='select_all']").onclick = function () {
         var items = document.querySelectorAll("#tag-list input[type='checkbox']")
@@ -168,11 +182,11 @@
         exportTags(`#tag-list input[type='checkbox']:checked`)
     };
 
-    document.getElementById("set-weight").onchange = function (e){
-        if(e.target.checked){
-            document.querySelectorAll("#tag-list input[type='number']").forEach((e)=>e.hidden=false)
-        }else{
-            document.querySelectorAll("#tag-list input[type='number']").forEach((e)=>e.hidden=true)
+    document.getElementById("set-weight").onchange = function (e) {
+        if (e.target.checked) {
+            document.querySelectorAll("#tag-list input[type='number']").forEach((e) => e.hidden = false)
+        } else {
+            document.querySelectorAll("#tag-list input[type='number']").forEach((e) => e.hidden = true)
         }
     }
 
